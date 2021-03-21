c1ceda373776705778e127f71771058965012e4b
SangTengkorak <62349215+SangTengkorak@users.noreply.github.com>
2021-03-21 23:27:49 +0700

    Update proto_local-task.sh

--- proto_local-task.sh
+++ proto_local-task.sh
@@ -2,10 +2,14 @@
 # SangTengkorak
 # March 21 2021
 
+# Ask user to define remote host to run commands
 read -p "Where you want to run command : " remhost
+
+# Create logfile, with time based name. Date and HourMinute parameters.
 saat=$(date +'%Y-%m-%d:%R')
 touch $saat.log
 
+# Read commands form file, and run it in remote host using SSH protocol.
 while read baris; do 
   ssh $remhost $baris > $saat.log
 done < simple_task.txt
